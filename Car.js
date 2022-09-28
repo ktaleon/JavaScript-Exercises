362bcc1eeccd303870627fac0844b3d52bb91b46
ktaleon <113583192+ktaleon@users.noreply.github.com>
2022-09-28 19:04:48 +0800

    Car

---
+++ Car.js
@@ -0,0 +1,9 @@
+const manu = document.getElementById("manufacturer"),
+      mnm = document.getElementById("make"),
+      eng = document.getElementById("engine"),
+      style = document.getElementById("bstyle");
+
+manu.innerHTML = "Mistubishi";
+mnm.innerHTML = "Lancer Evolution Final Edition"
+eng.innerHTML = "2.0L"
+style.innerHTML = "Sedan"
